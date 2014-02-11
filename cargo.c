035489fa8c8b8adfe728f4387a26d5d6013c026c
Joakim Söderberg <joakim.soderberg@gmail.com>
2014-02-11 01:17:37 +0100

    Some more addition to adding options.

--- cargo.c
+++ cargo.c
@@ -7,13 +7,31 @@
 
 typedef struct cargo_opt_s
 {
-
+	const char *name;
+	const char *description;
+	cargo_type_t type;
+	int nargs;
+	int alloc;
+	void *target;
+	size_t *target_count;
+	void *default_value;
 } cargo_opt_t;
 
 typedef struct cargo_s
 {
 	const char *progname;
 	const char *description;
+	cargo_format_t format;
+
+	int add_help;
+
+	cargo_opt_t *options;
+	size_t opt_count;
+	size_t max_opts;
+
+	char *args;
+	size_t arg_count;
+	size_t max_args;
 } cargo_s;
 
 static size_t _cargo_get_type_size(cargo_type_t t)
@@ -21,7 +39,6 @@ static size_t _cargo_get_type_size(cargo_type_t t)
 	switch (t)
 	{
 		default:
-		case CARGO_DEFAULT:
 		case CARGO_BOOL: 
 		case CARGO_INT: return sizeof(int);
 		case CARGO_UINT: return sizeof(unsigned int);
@@ -31,16 +48,49 @@ static size_t _cargo_get_type_size(cargo_type_t t)
 	}
 }
 
+static int _cargo_nargs_is_valid(int nargs)
+{
+	return (nargs >= 0) 
+		|| (nargs == CARGO_NARGS_NONE_OR_MORE)
+		|| (nargs == CARGO_NARGS_ONE_OR_MORE);
+}
+
 static int _cargo_add(cargo_t ctx,
 				const char *opt,
+				void *target,
+				size_t *target_count,
 				int nargs,
 				void *default_value,
-				void *target,
 				cargo_type_t type,
 				const char *description,
 				int alloc)
 {
-	size_t tsize = _cargo_get_type_size(type);
+	cargo_opt_t *o = NULL;
+
+	if (!_cargo_nargs_is_valid(nargs))
+		return -1;
+
+	if (!target)
+		return -1;
+
+	if (!target_count && (nargs > 0))
+		return -1;
+
+	if (ctx->opt_count >= ctx->max_opts)
+		return -1;
+
+	o = &ctx->options[ctx->opt_count];
+	ctx->opt_count++;
+
+	o->name = opt;
+	o->nargs = nargs;
+	o->target = target;
+	o->type = type;
+	o->description = description;
+	o->default_value = default_value;
+	o->target_count = target_count;
+	o->alloc = alloc;
+
 	return 0;
 }
 
@@ -48,9 +98,32 @@ static int _cargo_add(cargo_t ctx,
 // Public functions
 // -----------------------------------------------------------------------------
 
-int cargo_init(cargo_t *ctx, const char *progname, const char *description)
+int cargo_init(cargo_t *ctx, size_t max_opts, size_t max_args,
+				const char *progname, const char *description)
 {
+	cargo_s *c;
 	assert(ctx);
+
+	*ctx = (cargo_s *)calloc(1, sizeof(cargo_s));
+	c = *ctx;
+
+	if (!c)
+		return -1;
+
+	c->max_opts = max_opts;
+	c->max_args = max_args;
+
+	if (!(c->options = (cargo_opt_t *)calloc(max_opts, sizeof(cargo_opt_t))))
+	{
+		free(*ctx);
+		*ctx = NULL;
+		return -1;
+	}
+
+	c->progname = progname;
+	c->description = description;
+	c->add_help = 1;
+
 	return 0;
 }
 
@@ -68,35 +141,56 @@ void cargo_set_epilog(cargo_t ctx, const char *epilog)
 void cargo_add_help(cargo_t ctx, int add_help)
 {
 	assert(ctx);
+	ctx->add_help = add_help;
 }
 
 void cargo_set_format(cargo_t ctx, cargo_format_t format)
 {
 	assert(ctx);
+	ctx->format = format;
 }
 
 int cargo_add(cargo_t ctx,
 				const char *opt,
+				void *target,
+				size_t *target_count,
 				int nargs,
 				void *default_value,
-				void *target,
 				cargo_type_t type,
 				const char *description)
 {
 	assert(ctx);
-	return _cargo_add(ctx, opt, nargs, default_value, 
-						target, type, description, 0);
+	return _cargo_add(ctx, opt, target, target_count, nargs, 
+						default_value, type, description, 0);
 }
 
 int cargo_add_alloc(cargo_t ctx,
 				const char *opt,
+				void *target,
+				size_t *target_count,
 				int nargs,
 				void *default_value,
-				void *target,
 				cargo_type_t type,
 				const char *description)
 {
 	assert(ctx);
-	return _cargo_add(ctx, opt, nargs, default_value, 
-						target, type, description, 1);
+	return _cargo_add(ctx, opt, target, target_count, nargs, 
+						default_value, type, description, 1);
 }
+
+int cargo_parse(cargo_t ctx, int argc, char **argv)
+{
+	return 0;
+}
+
+#ifdef CARGO_TEST
+
+int main(int argc, char **argv)
+{
+
+
+	return 0;
+}
+
+#endif
+
--- cargo.h
+++ cargo.h
@@ -2,6 +2,8 @@
 #ifndef __CARGO_H__
 #define __CARGO_H__
 
+#include <stdio.h>
+
 typedef enum cargo_format_e
 {
 	CARGO_FORMAT_RAW_HELP = (1 << 0),
@@ -11,13 +13,12 @@ typedef enum cargo_format_e
 
 typedef enum cargo_type_e
 {
-	CARGO_DEFAULT = 0,
-	CARGO_BOOL = 1,
-	CARGO_INT = 2,
-	CARGO_UINT = 3,
-	CARGO_FLOAT = 4,
-	CARGO_DOUBLE = 5,
-	CARGO_STRING = 6
+	CARGO_BOOL = 0,
+	CARGO_INT = 1,
+	CARGO_UINT = 2,
+	CARGO_FLOAT = 3,
+	CARGO_DOUBLE = 4,
+	CARGO_STRING = 5
 } cargo_type_t;
 
 typedef struct cargo_s *cargo_t;
@@ -25,7 +26,10 @@ typedef struct cargo_s *cargo_t;
 #define CARGO_NARGS_ONE_OR_MORE -1
 #define CARGO_NARGS_NONE_OR_MORE -2
 
-int cargo_init(cargo_t *ctx, const char *progname, const char *description);
+int cargo_init(cargo_t *ctx, size_t max_opts, size_t max_args, 
+				const char *progname, const char *description);
+
+int cargo_destroy(cargo_t *ctx);
 
 void cargo_set_description(cargo_t ctx, const char *description);
 
@@ -37,22 +41,24 @@ void cargo_set_format(cargo_t ctx, cargo_format_t format);
 
 int cargo_add(cargo_t ctx,
 				const char *opt,
+				void *target,
+				size_t *target_count,
 				int nargs,
 				void *default_value,
-				void *target,
 				cargo_type_t type,
 				const char *description);
 
 int cargo_add_alloc(cargo_t ctx,
 				const char *opt,
+				void *target,
+				size_t *target_count,
 				int nargs,
 				void *default_value,
-				void *target,
 				cargo_type_t type,
 				const char *description);
 
+int cargo_parse(cargo_t ctx, int argc, char **argv);
 
 
-int cargo_destroy(cargo_t *ctx);
 
 #endif // __CARGO_H__
