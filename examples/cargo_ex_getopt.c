1eec9ac80ccec82db753d3493bf6b0f2c1395633
Joakim Söderberg <joakim.soderberg@gmail.com>
2015-04-10 10:09:43 +0200

    Fix build

--- examples/cargo_ex_getopt.c
+++ examples/cargo_ex_getopt.c
@@ -1,3 +1,26 @@
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Joakim Soderberg <joakim.soderberg@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -38,7 +61,7 @@ typedef struct args_s
 static int print_command(cargo_t ctx, void *user, const char *optname,
 						int argc, char **argv)
 {
-	args_t *a = (args_t *)cargo_get_user_context(ctx);
+	args_t *a = (args_t *)cargo_get_context(ctx);
 	assert(a);
 
 	a->cmd = (cmd_t)user;
@@ -63,7 +86,7 @@ int main(int argc, char **argv)
 	}
 
 	memset(&args, 0, sizeof(args));
-	cargo_set_user_context(cargo, &args);
+	cargo_set_context(cargo, &args);
 
 	ret |= cargo_add_option(cargo, 0, "--verbose -v", "Be more verbose",
 							"b!", &args.verbose_flag);
@@ -106,6 +129,8 @@ int main(int argc, char **argv)
 
 		printf("\n");
 	}
+
+	cargo_destroy(&cargo);
 
 	return 0;
 }
