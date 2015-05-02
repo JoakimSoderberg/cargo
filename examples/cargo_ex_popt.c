cf664788deacc2722c5245b9a133d1f33ca54e61
Joakim Söderberg <joakim.soderberg@gmail.com>
2015-05-02 15:44:09 +0200

    Update popt example to use CARGO_OPT_STOP instead
    this way is a lot cleaner

--- examples/cargo_ex_popt.c
+++ examples/cargo_ex_popt.c
@@ -27,6 +27,10 @@
 #include <string.h>
 #include "cargo.h"
 
+#ifdef _WIN32
+  #define strcasecmp _stricmp
+#endif
+
 static char *og1_1 = "default 1 1";
 
 static char *og2_1 = "default 2 1";
@@ -46,9 +50,9 @@ typedef enum cmd_s
 
 static cmd_t str_to_cmd(const char *str)
 {
-	if (!strcmp(str, "command1")) return COMMAND1;
-	if (!strcmp(str, "command2")) return COMMAND2;
-	if (!strcmp(str, "command3")) return COMMAND3;
+	if (!strcasecmp(str, "command1")) return COMMAND1;
+	if (!strcasecmp(str, "command2")) return COMMAND2;
+	if (!strcasecmp(str, "command3")) return COMMAND3;
 
 	return INVALID_COMMAND;
 }
@@ -69,12 +73,22 @@ int add_module1_opts(cargo_t cargo)
 	return ret;
 }
 
-int subcommand1(int argc, char **argv)
+void print_args(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
+
+int subcommand1(int start, int argc, char **argv)
 {
 	int ret = 0;
 	cargo_t mod1;
-
-	if (cargo_init(&mod1, 0, argv[0]))
+	if (!argv) return -1;
+	if (cargo_init(&mod1, 0, "%s command1", argv[0]))
 	{
 		fprintf(stderr, "Failed to init command line parsing\n");
 		return -1;
@@ -84,15 +98,25 @@ int subcommand1(int argc, char **argv)
 	ret |= add_module1_opts(mod1);
 	assert(ret == 0);
 
+	print_args(argc - start, &argv[start]);
+
+	if (cargo_parse(mod1, 0, start, argc, argv)) return -1;
+
+	printf("opt1 == %s\n", global_1);
+
+	cargo_destroy(&mod1);
+
 	return ret;
 }
 
-int subcommand2(int argc, char **argv)
+int subcommand2(int start, int argc, char **argv)
 {
 	int ret = 0;
 	cargo_t mod2;
 
-	if (cargo_init(&mod2, 0, argv[0]))
+	if (!argv) return -1;
+
+	if (cargo_init(&mod2, 0,  "%s command2", argv[0]))
 	{
 		fprintf(stderr, "Failed to init command line parsing\n");
 		return -1;
@@ -108,6 +132,8 @@ int subcommand2(int argc, char **argv)
 	ret |= cargo_add_option(mod2, 0, "<module2> --og2_1", "String used for option 1 1", "s", &og1_1);
 	assert(ret == 0);
 
+	cargo_destroy(&mod2);
+
 	return ret;
 }
 
@@ -120,7 +146,7 @@ static int parse_command_cb(cargo_t ctx, void *user, char *optname, int argc, ch
 
 	if (*cmd == INVALID_COMMAND)
 	{
-		fprintf(stderr, "Invalid command \"%s\"\n", argv[0]);
+		cargo_set_error(ctx, 0, "Invalid command \"%s\"\n", argv[0]);
 		return -1;
 	}
 	return 1;
@@ -131,12 +157,14 @@ int main(int argc, char **argv)
 	cargo_t cargo;
 	int ret = 0;
 	size_t i;
-	char **strs = NULL;
-	size_t str_count = 0;
+	char **args = NULL;
+	size_t args_count = 0;
 	int verbose = 0;
+	int help = 0;
+	int stop_index = 0;
 	cmd_t cmd = INVALID_COMMAND;
 
-	if (cargo_init(&cargo, 0, argv[0]))
+	if (cargo_init(&cargo, CARGO_AUTOCLEAN | CARGO_NO_AUTOHELP, argv[0]))
 	{
 		fprintf(stderr, "Failed to init command line parsing\n");
 		return -1;
@@ -151,52 +179,82 @@ int main(int argc, char **argv)
 			"cmds", "Commands", NULL);
 	ret |= cargo_mutex_group_set_metavar(cargo, "cmds", "COMMAND");
 
-	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED, "<!cmds> command1",
+	// This option will stop the parsing.
+	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED | CARGO_OPT_STOP,
+			"<!cmds> command1",
 			"Silly example command", "c", parse_command_cb, &cmd);
-
-	ret |= cargo_add_option(cargo, 0, "args",
-			"Some more args", "[s]*", &strs, &str_count);
-
 
 	// This is just a dummy, it will not parse anything that is left to command1
 	// but still show up in usage and so on.
-	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED, "<!cmds> command2",
+	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED,
+			"<!cmds> command2",
 			"Another silly example command", "c0", NULL, NULL);
 
-	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED, "<!cmds> command3",
+	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED,
+			"<!cmds> command3",
 			"The third silly command", "D");
 
 	//
 	// Some other arguments.
 	//
 
+	ret |= cargo_add_option(cargo, 0, "--help --usage -h", NULL, "b", &help);
+	ret |= cargo_set_option_description(cargo, "--help",
+			"Show this help.                         \n"
+			"To get help for any of the commands:\n"
+			" %s COMMAND --help", argv[0]);
+
 	// Allow -vvv or multiple --verbosity and -v are counted to raise the count.
-	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED, "--verbose -v",
+	ret |= cargo_add_option(cargo, 0, "--verbose -v",
 			"Verbosity", "b!", &verbose);
+
+	ret |= cargo_add_option(cargo, CARGO_OPT_NOT_REQUIRED, "args",
+			"Some more args", "D", &args, &args_count);
+	ret |= cargo_set_metavar(cargo, "args", "ARGS ...");
 
 	assert(ret == 0);
 
-	if (cargo_parse(cargo, 0, 1, argc, argv)) return -1;
+	if (cargo_parse(cargo, CARGO_NOERR_OUTPUT,
+					1, argc, argv))
+	{
+		if (help)
+		{
+			cargo_print_usage(cargo, 0);
+			return 0;
+		}
+
+		fprintf(stderr, "%s\n", cargo_get_error(cargo));
+		return -1;
+	}
+
+	if (help)
+	{
+		cargo_print_usage(cargo, 0);
+		return 0;
+	}
+
+	stop_index = cargo_get_stop_index(cargo);
+	printf("Stop index: %d\n", stop_index);
 
 	switch (cmd)
 	{
 		default:
 		case INVALID_COMMAND: printf("Invalid command\n"); break;
-		case COMMAND1: printf("Command 1\n"); break;
-		case COMMAND2: printf("Command 2\n"); break;
+		case COMMAND1: printf("Command 1\n"); subcommand1(stop_index, argc, argv); break;
+		case COMMAND2: printf("Command 2\n"); subcommand2(stop_index, argc, argv); break;
 		case COMMAND3: printf("Command 3\n"); break;
 	}
 
 	if (verbose)
 	{
-		printf("Got %lu extra arguments\n", str_count);
+		printf("Got %lu extra arguments\n", args_count);
 	}
 
 	if (verbose >= 2)
 	{
-		for (i = 0; i < str_count; i++)
+		for (i = 0; i < args_count; i++)
 		{
-			printf("%s\n", strs[i]);
+			printf("%s\n", args[i]);
 		}
 	}
 
