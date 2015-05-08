664a7dba7f1637cd8ef6aeb62a63e0a0d63dbd5f
Joakim Söderberg <joakim.soderberg@gmail.com>
2015-05-08 22:31:23 +0200

    More examples.

--- examples/cargo_ex_mutex.c
+++ examples/cargo_ex_mutex.c
@@ -40,12 +40,6 @@ int main(int argc, char **argv)
 {
     int ret = 0;
     cargo_t cargo;
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int d = 0;
-    int e = 0;
-    int f = 0;
 
     if (cargo_init(&cargo, 0, argv[0]))
     {
@@ -53,44 +47,57 @@ int main(int argc, char **argv)
         return -1;
     }
 
+    cargo_set_description(cargo, LOREM_IPSUM);
+
     // Default group.
-    ret |= cargo_add_option(cargo, 0, "--beta -b", "The beta thing", "i", &b);
-    ret |= cargo_add_option(cargo, 0, "--centauri -c", LOREM_IPSUM, "i", &c);
+    ret |= cargo_add_option(cargo, 0, "--alpha -a", "Alpha flag", "D");
+    ret |= cargo_add_option(cargo, 0, "--beta -b", "The beta thing", "D");
+    ret |= cargo_add_option(cargo, 0, "--centauri -c", LOREM_IPSUM, "D");
 
-    // First group.
+    // First mutex group.
+    ret |= cargo_add_mutex_group(cargo, 0, "mgroup1", "Mutex group one", NULL);
+    ret |= cargo_add_option(cargo, 0, "<!mgroup1> --delta -d", LOREM_IPSUM, "D");
+    ret |= cargo_add_option(cargo, 0, "<!mgroup1> --elastic -e", "Elastic", "D");
+    assert(ret == 0);
+
+    // First normal group.
     ret |= cargo_add_group(cargo, 0, "group1",
-                    "Group one", "Here are some extra flags.");
-    ret |= cargo_add_option(cargo, 0, "<group1> --delta -d",
-                            "The delta " LOREM_IPSUM, "b", &d);
-    ret |= cargo_add_option(cargo, 0, "<group1> --elastic -e",
-                            "The elastic rubber band", "b", &e);
+            "Group one", "This is an extra group. " LOREM_IPSUM);
+    ret |= cargo_add_option(cargo, 0, "<group1> --fact -f", LOREM_IPSUM, "D");
+    ret |= cargo_add_option(cargo, 0, "<group1> --great -g", LOREM_IPSUM, "D");
+    ret |= cargo_add_option(cargo, 0, "<group1> --hello", "Hello there", "D");
+
+    // Second mutex group.
+    // (Add existing options to the group).
+    ret |= cargo_add_mutex_group(cargo,
+            CARGO_MUTEXGRP_ONE_REQUIRED,
+            "mgroup2", "Mutex group two", NULL);
+    ret |= cargo_mutex_group_add_option(cargo, "mgroup2", "--delta");
+    ret |= cargo_mutex_group_add_option(cargo, "mgroup2", "--great");
     assert(ret == 0);
 
-    // Second group.
-    ret |= cargo_add_group(cargo, 0, "group2",
-                    "Group two", "This is an extra group. " LOREM_IPSUM);
-    ret |= cargo_add_option(cargo, 0, "<group2> --fact -f", LOREM_IPSUM, "i", &f);
-    ret |= cargo_add_option(cargo, 0, "<group2> --great -g", LOREM_IPSUM, "D");
-    ret |= cargo_add_option(cargo, 0, "<group2> --hello", "Hello there", "D");
+    // Third mutex group.
+    // (Group the option in the usage like a normal group
+    //  but not in the short usage)
+    ret |= cargo_add_mutex_group(cargo,
+            CARGO_MUTEXGRP_GROUP_USAGE |
+            CARGO_MUTEXGRP_NO_GROUP_SHORT_USAGE,
+            "mgroup3", "Mutex group three", "Only one of these allowed.");
+    ret |= cargo_add_option(cargo, 0, "<!mgroup3> --ignore -i", "Ignore", "D");
+    ret |= cargo_add_option(cargo, 0, "<!mgroup3> --joke -j", "Joke", "D");
 
+    // Add an existing option to this group, it will be "stolen" from group1.
+    ret |= cargo_mutex_group_add_option(cargo, "mgroup3", "--fact");
+
+    cargo_set_epilog(cargo, "This is a text at the end a so called %s", "epilog");
     assert(ret == 0);
-
-    if (argc < 2)
-    {
-        cargo_print_usage(cargo, CARGO_USAGE_SHORT_USAGE);
-    }
 
     if (cargo_parse(cargo, 0, 1, argc, argv))
     {
         return -1;
     }
 
-    printf("Alpha: %d\n", a);
-    printf("Beta: %d\n", b);
-    printf("Centauri: %d\n", c);
-    printf("Delta: %d\n", d);
-    printf("Elastic: %d\n", e);
-
+fail:
     cargo_destroy(&cargo);
 
     return 0;
